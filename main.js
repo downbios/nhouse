970cad15c1f957e2b9b7574508d2f0e8eb6e59f6
Ian Adson <58149052+downbios@users.noreply.github.com>
2021-11-18 23:06:57 -0300

    media querys/ active menu / responsividade

--- main.js
+++ main.js
@@ -15,15 +15,40 @@ for (const link of links) {
   })
 }
 /* Sombrear header Scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
 }
+/*  Active Menu Section Current */
+const sections = document.querySelector('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
 /*  When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
@@ -35,7 +60,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 /* Scrollreveal Preview */
 const scrollReveal = ScrollReveal({
@@ -47,7 +78,7 @@ const scrollReveal = ScrollReveal({
 scrollReveal.reveal(
   `#home .image, #home .text,
   #about .image, #about .text,
-  #services header, #services .card,
+  #services header, #services .cards,
   #testimonials header, #testimonials .testimonial,
   #contact .text, #contact .links`,
   { interval: 100 }

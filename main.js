2fb70472aa986315de9a142124823ad399c00308
ian adson <ian.adson.vieira@gmail.com>
2024-04-12 09:36:53 -0300

    first commit

--- main.js
+++ main.js
@@ -1,85 +1,55 @@
 /* Interruptor Menu*/
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+const nav = document.querySelector("#header nav");
+const toggle = document.querySelectorAll("nav .toggle");
 
 for (const element of toggle) {
-  element.addEventListener('click', function () {
-    nav.classList.toggle('show')
-  })
+  element.addEventListener("click", function () {
+    nav.classList.toggle("show");
+  });
 }
-const links = document.querySelectorAll('nav ul li a')
+const links = document.querySelectorAll("nav ul li a");
 
 for (const link of links) {
-  link.addEventListener('click', function () {
-    nav.classList.remove('show')
-  })
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
+  });
 }
 /* Sombrear header Scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+const header = document.querySelector("#header");
+const navHeight = header.offsetHeight;
 function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
-    header.classList.add('scroll')
+    header.classList.add("scroll");
   } else {
-    header.classList.remove('scroll')
+    header.classList.remove("scroll");
   }
 }
 /*  Active Menu Section Current */
-const sections = document.querySelector('main section[id]')
+const sections = document.querySelector("main section[id]");
 function activateMenuAtCurrentSection() {
-  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
 
   for (const section of sections) {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-    const sectionId = section.getAttribute('id')
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
 
-    const checkpointStart = checkpoint >= sectionTop
-    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
 
     if (checkpointStart && checkpointEnd) {
       document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.add('active')
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
     } else {
       document
-        .querySelector('nav ul li a[href*=' + sectionId + ']')
-        .classList.remove('active')
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
     }
   }
 }
 
 /*  When Scroll */
-window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll()
-})
-/* testimony carrousel slider swiper */
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 1,
-  pagination: {
-    el: '.swiper-pagination'
-  },
-  mousewheel: true,
-  keyboard: true,
-  breakpoints: {
-    767: {
-      slidesPerView: 2,
-      setWrapperSize: true
-    }
-  }
-})
-/* Scrollreveal Preview */
-const scrollReveal = ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  duration: 700,
-  reset: true
-})
-scrollReveal.reveal(
-  `#home .image, #home .text,
-  #about .image, #about .text,
-  #services header, #services .cards,
-  #testimonials header, #testimonials .testimonial,
-  #contact .text, #contact .links`,
-  { interval: 100 }
-)
+window.addEventListener("scroll", function () {
+  changeHeaderWhenScroll();
+});
